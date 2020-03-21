139039c4039b73c4b4632d75aff999d9d4325a2a
Paolo Soncco <paholo2054@gmail.com>
2020-03-21 00:46:30 -0500

    primero

---
+++ precache-manifest.26bee26c2f84c7592ab77db46aecb5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf8aae12e60fedf3536528b66bcf67e",
+    "url": "/makemoney/index.html"
+  },
+  {
+    "revision": "31b2125a21349cc9ffab",
+    "url": "/makemoney/static/css/main.6280febe.chunk.css"
+  },
+  {
+    "revision": "b7ddcbeb2b37494c3e80",
+    "url": "/makemoney/static/js/2.532069ec.chunk.js"
+  },
+  {
+    "revision": "fff893798278dc57ce07f24dd8c43a2b",
+    "url": "/makemoney/static/js/2.532069ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b2125a21349cc9ffab",
+    "url": "/makemoney/static/js/main.2723ee80.chunk.js"
+  },
+  {
+    "revision": "0525020f2f4717f3acdf",
+    "url": "/makemoney/static/js/runtime-main.a0480918.js"
+  },
+  {
+    "revision": "c52c69467d157c2248480b66a5a2b4cf",
+    "url": "/makemoney/static/media/calavera.c52c6946.jpg"
+  }
+]);
